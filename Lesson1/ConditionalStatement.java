ac46b79f20286f076d228f06169312a8d5ca141d
Evgeniy Titov <ewgeniy.titov@yandex.ru>
2019-03-09 12:09:25 +0300

    Dobavil ConditionalStatement

--- Lesson1/ConditionalStatement.java
+++ Lesson1/ConditionalStatement.java
@@ -1,7 +1,7 @@
 public class ConditionalStatement {
     public static void main(String[] args) {
         int age = 25;
-                if (age > 20) {
+        if (age > 20) {
             System.out.println("Vozrast bol'she 20 let");
         }
 
