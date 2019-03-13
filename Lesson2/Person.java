0d9ff4eac970bd0702d0e89fc083df6f2d721452
Evgeniy Titov <ewgeniy.titov@yandex.ru>
2019-03-13 20:54:45 +0300

    add Person.java

---
+++ Lesson2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    boolean isMale = true;
+    String personName = "Evgeniy";
+    int growth = 183;
+    int weight = 80;
+    int age = 25;
+
+    void go () {
+
+    }
+
+    void sit () {
+
+    }
+
+    void run () {
+
+    }
+
+    void say () {
+
+    }
+
+    void learnJava () {
+
+    }
+}
