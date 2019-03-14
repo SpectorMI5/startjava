4e487aaf6c8c9399a59befafbc3eda49f2ce468e
Evgeniy Titov <ewgeniy.titov@yandex.ru>
2019-03-14 16:22:20 +0300

    Add Wolf.java

---
+++ Lesson2/Wolf.java
@@ -0,0 +1,25 @@
+public class Wolf {
+
+    String gender;
+    String nickname;
+    int weight;
+    int age;
+    String colour;
+
+    void go () {
+        System.out.println("Wolf walked 10 meters");
+    }
+    void sit () {
+        System.out.println("Wolf sat down");
+    }
+    void run () {
+        System.out.println("Wolf ran 20 meters");
+    }
+    void howl () {
+        System.out.println("UU-UU-UU");
+    }
+    boolean hunt () {
+        System.out.println("Wolf got prey");
+        return true;
+    }
+}
