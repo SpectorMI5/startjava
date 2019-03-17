5a50b09043d0d8691a3188580ff7da4ea43514d0
Evgeniy Titov <ewgeniy.titov@yandex.ru>
2019-03-17 13:09:39 +0300

    Add Jaeger.java & JaegerTest.java

---
+++ Lesson2/Jaeger.java
@@ -0,0 +1,81 @@
+public class Jaeger {
+
+    private String modelName;
+    private String mark;
+    private float height;
+    private int weight;
+    private int speed;
+    private int strength;
+    private int armor;
+    private int length;
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public void move() {
+        length = speed;
+        System.out.println(modelName + " walked " + length + " steps");
+    }
+
+    public void scanKaiju() {
+        System.out.println(modelName + " found nothing");
+    }
+
+    public void run() {
+        length = speed * 3;
+        System.out.println(modelName + " run " + length + " steps");
+    }
+}
---
+++ Lesson2/JaegerTest.java
@@ -0,0 +1,54 @@
+public class JaegerTest {
+
+    public static void main(String[] args) {
+        Jaeger jaegerOne = new Jaeger();
+        jaegerOne.setModelName("Coyote Tango");
+        jaegerOne.setMark("Mark-1");
+        jaegerOne.setHeight(85.34f);
+        jaegerOne.setWeight(2312);
+        jaegerOne.setSpeed(5);
+        jaegerOne.setStrength(7);
+        jaegerOne.setArmor(4);
+
+        Jaeger jaegerTwo = new Jaeger();
+        jaegerTwo.setModelName("Striker Eureka");
+        jaegerTwo.setMark("Mark-5");
+        jaegerTwo.setHeight(76.2f);
+        jaegerTwo.setWeight(1850);
+        jaegerTwo.setSpeed(10);
+        jaegerTwo.setStrength(10);
+        jaegerTwo.setArmor(9);
+
+        System.out.println("Model name: " + jaegerOne.getModelName());
+        System.out.println("Mark: " + jaegerOne.getMark());
+        System.out.println("Height: " + jaegerOne.getHeight() + " m");
+        System.out.println("Weight: " + jaegerOne.getWeight() + " tons");
+        System.out.println("Speed: " + jaegerOne.getSpeed());
+        System.out.println("Strength: " + jaegerOne.getStrength());
+        System.out.println("Armor: " + jaegerOne.getArmor() + "\n");
+
+        System.out.println("Model name: " + jaegerTwo.getModelName());
+        System.out.println("Mark: " + jaegerTwo.getMark());
+        System.out.println("Height: " + jaegerTwo.getHeight() + " m");
+        System.out.println("Weight: " + jaegerTwo.getWeight() + " tons");
+        System.out.println("Speed: " + jaegerTwo.getSpeed());
+        System.out.println("Strength: " + jaegerTwo.getStrength());
+        System.out.println("Armor: " + jaegerTwo.getArmor() + "\n");
+
+        jaegerOne.move();
+        jaegerOne.scanKaiju();
+        jaegerOne.run();
+        jaegerTwo.move();
+        jaegerTwo.scanKaiju();
+        jaegerTwo.run();
+
+        jaegerOne.setWeight(2174);
+        System.out.println("\nWeight: " + jaegerOne.getWeight() + " tons");
+        jaegerOne.setSpeed(6);
+        jaegerOne.move();
+        jaegerTwo.setModelName("Defender Eureka");
+        System.out.println("Model name: " + jaegerTwo.getModelName());
+        jaegerTwo.setHeight(81.62f);
+        System.out.println("Height: " + jaegerTwo.getHeight() + " m");
+    }
+}
